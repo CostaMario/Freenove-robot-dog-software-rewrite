f525af52de727bc777d817f2589bad50bf7e1785
Mario Costa <cost.mario08@gmail.com>
2024-10-16 22:03:33 +0200

    Added systemd service, reduced cmake version, added github workflow

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir -p build
 cmake -DCMAKE_TOOLCHAIN_FILE=../robot-src/Toolchain-RaspberryPi.cmake robot-src -B build
 cmake --build build
+chmod +x build/robot-server
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+winget https://github.com/CostaMario/Freenove-robot-dog-software-rewrite/releases/download/dev/robot-server
+cp robot-server /usr/bin/local/robot-server
